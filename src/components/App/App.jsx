c45e8df561ef40942f4ed6133730fbe43734317b
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-16 11:13:14 -0500

    created PUT route to change ispurchased status to false for all items

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import ListForm from '../ListForm/ListForm.jsx';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
@@ -40,7 +39,6 @@ function App() {
         <div className="App">
             <Header />
             <main>
-            <ListForm />
                 <p>Under Construction...</p>
             </main>
         </div>
