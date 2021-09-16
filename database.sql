e29b6abb1f7b847f56783d7a2a3c84e7437ea482
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-16 10:07:47 -0500

    added database in database.sql

--- database.sql
+++ database.sql
@@ -1,2 +1,19 @@
--- Don't forget to add your create table SQL 
--- It is also helpful to include some test data
+-- CREATE DATABASE "fs-react-shopping";
+
+-- Table structure
+CREATE TABLE list (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80) NOT NULL,
+	quantity DECIMAL(3, 2) NOT NULL,
+	unit VARCHAR(20),
+	isPurchased BOOLEAN DEFAULT false
+);
+
+INSERT INTO list (name, quantity, unit)
+VALUES ('Orange Juice', '2', 'bottles'), 
+('Apples', '5', 'lbs'), 
+('Bread', '1', 'loaf'),
+('Milk', '1', 'gallon'),
+('Sliced Almonds', '2', 'cups');
+
+SELECT * FROM list;
