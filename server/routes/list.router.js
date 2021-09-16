aab544af23fab0e1227e51ec7b74713b03babaa0
Sarah Fuoss <sfuoss@gmail.com>
2021-09-16 14:09:19 -0500

    Fixed typo on delete request.

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -70,7 +70,7 @@ router.delete('/list', (req, res) => {
     const queryText = `DELETE * FROM "list";`
     pool.query(queryText).then(result => {
         res.sendStatus(200);
-    }).catch.catch(error => {
+    }).catch.(error => {
         console.log('error in deleting database');
         res.sendStatus(500);
     })
